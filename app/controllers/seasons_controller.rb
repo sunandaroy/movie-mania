89ece80f54ffc170acdbc132f190ebcf9c2fcbbf
sunandaroy <sunanda.roy06@gmail.com>
2020-04-27 17:24:00 +0530

    sort_by added for episodes by number

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,12 +2,12 @@ class SeasonsController < ApplicationController
 
   def index
     @seasons = Season.includes(:episodes).all
-    @season = @seasons.sort_by(&:created_at)
+    @seasons = @seasons.sort_by!(&:created_at)
     @seasons.each{|s|
-      @episodes =s.episodes
-      s[:episodes] = @episodes
+      season_episodes = s.episodes
+      season_episodes = season_episodes.sort_by!(&:number)
+      s[:episodes] = season_episodes
     }
-
     respond_to do |format|
       format.json {render :json => @seasons}
     end
