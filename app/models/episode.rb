b9850868a15d12a1eb7a4b0b6e7e04eb4018d766
sunandaroy <sunanda.roy06@gmail.com>
2020-04-27 16:27:08 +0530

    adding validation for episode active record

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,5 @@
 class Episode < ActiveRecord::Base
+  validates :title, :number, :presence => true
   belongs_to :season
-  attr_accessible :plot, :title, :season_id
+  attr_accessible :plot, :title, :number, :season_id
 end
